8b67343d47b5dde6f47d010875fe8e473cdd96f8
GuilhermeMota20 <guidroidbr.2007@gmail.com>
2024-01-26 14:52:35 -0300

    User Dropdown

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -3,6 +3,7 @@ import { ChevronsLeft, MenuIcon } from "lucide-react";
 import { usePathname } from "next/navigation";
 import React, { ElementRef, useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
+import UserItem from "./user-item";
 
 export default function Navigation() {
   const pathname = usePathname();
@@ -113,7 +114,7 @@ export default function Navigation() {
         </div>
 
         <div>
-          <p>Action itens</p>
+          <UserItem />
         </div>
 
         <div className="mt-4">
