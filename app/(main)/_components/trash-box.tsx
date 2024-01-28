a36e78b454ed825a1b6df42535b20a2888bf2998
GuilhermeMota20 <guidroidbr.2007@gmail.com>
2024-01-28 17:46:08 -0300

    Fix dark mode

--- app/(main)/_components/trash-box.tsx
+++ app/(main)/_components/trash-box.tsx
@@ -99,7 +99,7 @@ export default function TrashBox() {
                 <div
                   role="button"
                   onClick={(e) => onRestore(e, doc._id)}
-                  className="rounded-sm p-2 hover:bg-neutral-200"
+                  className="rounded-sm p-2 hover:bg-neutral-200 dark:hover:bg-neutral-600"
                 >
                   <Undo className="w-4 h-4 text-muted-foreground" />
                 </div>
@@ -108,7 +108,7 @@ export default function TrashBox() {
                 >
                   <div
                     role="button"
-                    className="rounded-sm p-2 hover:bg-neutral-200"
+                    className="rounded-sm p-2 hover:bg-neutral-200 dark:hover:bg-neutral-600"
                   >
                     <Trash className="w-4 h-4 text-muted-foreground" />
                   </div>
